a95455c69428fcd3dec1bf04a395e1c2f28e1898
Emerson Jordan <emerz210@gmail.com>
2024-01-07 15:20:32 -0800

    add write page with textbox

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from "react";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const Write = () => {
-    return (<div>Write</div>);
-}
- 
+    const [value, setValue] = useState('');
+    console.log(value);
+  return (
+    <div className="add">
+      <div className="content">
+        <input type="text" placeholder="title" />
+        <div className="editorContainer">
+        return <ReactQuill theme="snow" value={value} onChange={setValue} />
+        </div>
+      </div>
+      <div className="menu">
+        <div className="item">i1</div>
+        <div className="item">i2</div>
+      </div>
+    </div>
+  );
+};
+
 export default Write;
