8ae85c5e2595b3ad0b1c589ab98a2a097b24a2eb
Emerson Jordan <emerz210@gmail.com>
2024-01-07 15:07:53 -0800

    add styling

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,20 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Menu from '../components/Menu';
+import React from "react";
+import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
 
 const Single = () => {
-    return (
-    <div className='single'>
-        <div className="content">c</div>
-        <div className="menu">m</div>
-        <div className="edit"></div>
+  return (
+    <div className="single">
+      <div className="content">
+        <img
+          src="https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
 
-        <Link to={`/write?edit=2`}>Edit 2</Link>
-        <Menu/>
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <div className="info">
+            <span>John</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <h1>Lorem ipsum adsfadsf adsf </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum{" "}
+          </p>
+        </div>
+      </div>
+
+      <div className="menu">m</div>
+      <div className="edit">
+        <Link to={`/write?edit=2`}>
+          <img src="https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
+        </Link>
+        <Link>
+          <img src="https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
+        </Link>
+      </div>
+
+      <Link to={`/write?edit=2`}>Edit 2</Link>
+      <Menu />
     </div>
+  );
+};
 
-
-    );
-}
- 
 export default Single;
