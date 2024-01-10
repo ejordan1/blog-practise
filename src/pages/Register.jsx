fe8d15b2583ada4dcd48cab1954c0f229f346799
Emerson Jordan <emerz210@gmail.com>
2024-01-09 18:28:15 -0800

    add user registration in backend and frontend registration page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
-    return (<div>Register</div>);
+const [inputs, setInputs] = useState({
+    username:"",
+    email:"",
+    password:"",
+})
+
+    const handleChange = e =>{
+        setInputs(prev=>({...prev, [e.target.name]: e.target.value}))
+    }
+
+    console.log(inputs)
+
+    return (
+        <div className="auth">
+        <h1>Register</h1>
+    <form>
+        <input required type="text" placeholder='username' name='username' onChange={handleChange}/>
+        <input required type="email" placeholder='email' name='email' onChange={handleChange}/>
+        <input required type="password" placeholder='password'name='password' onChange={handleChange}/>
+        <button>Login</button>
+        <span>Doy you have an account <Link to="/login">Login</Link>
+        </span>
+    </form>
+    </div>);
 }
  
 export default Register;
