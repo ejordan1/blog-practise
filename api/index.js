d8541e76ea5737b00d60a55cba51d438b07fccb4
Emerson Jordan <emerz210@gmail.com>
2024-01-09 17:19:07 -0800

    add structure for routes and controller

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 import express from "express"
+import postRoutes from "./routes/posts.js"
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
 
 const app = express()
 
 app.use(express.json())
+app.use("/api/posts", postRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+
+app.get("/test", (req,res)=>{
+    res.json("It works!");
+})
 
 app.listen(8800, ()=>{
     console.log("Connected")
